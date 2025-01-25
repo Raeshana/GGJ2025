8e52ec2e35b6578587fe08ad2d55c316761dff6e
Raeshana <rsookhoo@andrew.cmu.edu>
2025-01-25 00:00:00 -0500

    Spawns bubbles at a random transform point, deletes bubbles after x seconds

--- Assets/Scripts/BubbleSpawn.cs
+++ Assets/Scripts/BubbleSpawn.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BubbleSpawn : MonoBehaviour
 {
     [SerializeField] GameObject bubblePrefab;
-    [SerializeField] float waitTime;
+    [SerializeField] float spawnInterval;
+    [SerializeField] float spawnLength;
     [SerializeField] Transform[] transforms;
     
     // Update is called once per frame
@@ -13,12 +14,35 @@ public class BubbleSpawn : MonoBehaviour
     {
         StartCoroutine(BubbleSpawnCoroutine());
     }
-
+    
+    /// <summary>
+    /// Spawns a bubble at a random point in transforms
+    /// every spawnInterval seconds
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator BubbleSpawnCoroutine()
     {
         while (true) {
-            yield return new WaitForSeconds(waitTime);
-            Instantiate(bubblePrefab, transforms[0].position, Quaternion.identity);
+            // Get random index in transforms
+            int point = Random.Range(0, transforms.Length-1); 
+
+            yield return new WaitForSeconds(spawnInterval);
+
+            // Instantiate bubble at transforms[index]
+            GameObject bubble = Instantiate(bubblePrefab, transforms[point].position, Quaternion.identity); 
+
+            // Reduces overhead by deleting bubble once offscrean
+            StartCoroutine(BubbleDestroyCoroutine(bubble));
         }
+    }
+
+    /// <summary>
+    /// Deletes bubble after spawnLength seconds
+    /// </summary>
+    /// <param name="bubble"> Spawned bubble </param>
+    /// <returns></returns>
+    private IEnumerator BubbleDestroyCoroutine(GameObject bubble) {
+        yield return new WaitForSeconds(spawnLength);
+        Destroy(bubble);
     }
 }

ed801e750bcaf66e00a4b0bde1bae2d3f7f653a4
Raeshana <rsookhoo@andrew.cmu.edu>
2025-01-26 14:46:04 -0500

    added counter

--- Assets/Scripts/Headlines/HeadlineCollision.cs
+++ Assets/Scripts/Headlines/HeadlineCollision.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HeadlineCollision : MonoBehaviour
 {
     public Headlines headlineMovement;
 
-    void Start() {
-        headlineMovement.blueScore = 0.5f;
-        Debug.Log("headlineCollision started");
-    }
+    public int blueCounter;
+    public int redCounter;
 
+    public TMP_Text blueText;
+    public TMP_Text redText;
+
+    void Start() {
+        //headlineMovement.blueScore = 0.5f;
+        Debug.Log("headlineCollision started");
+
+        blueCounter = 0;
+        redCounter = 0;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         // print the tag of the object that collided with the headline
         Debug.Log(gameObject.tag + "collided with: " + other.gameObject.tag);
         
-        // Check if the object that collides is a bubble
+        // // Check if the object that collides is a bubble
+        // if (other.CompareTag("Red") && gameObject.CompareTag("Red"))
+        // {
+        //     headlineMovement.blueScore--;
+        //     Debug.Log("Red bubble hit RedHeadline! Decreasing blueScore. New blueScore: " + headlineMovement.blueScore);
+        // }
+        // else if (other.CompareTag("Blue") && gameObject.CompareTag("Blue"))
+        // {
+        //     headlineMovement.blueScore++;
+        //     Debug.Log("Blue bubble hit BlueHeadline! Increasing blueScore. New blueScore: " + headlineMovement.blueScore);
+        // }
+
         if (other.CompareTag("Red") && gameObject.CompareTag("Red"))
         {
-            headlineMovement.blueScore--;
-            Debug.Log("Red bubble hit RedHeadline! Decreasing blueScore. New blueScore: " + headlineMovement.blueScore);
+            redCounter++;
+            redText.text = "" + redCounter; 
         }
         else if (other.CompareTag("Blue") && gameObject.CompareTag("Blue"))
         {
-            headlineMovement.blueScore++;
-            Debug.Log("Blue bubble hit BlueHeadline! Increasing blueScore. New blueScore: " + headlineMovement.blueScore);
+            blueCounter++;
+            blueText.text = "" + blueCounter; 
         }
 
         // Optionally, destroy the bubble after it interacts with the headline

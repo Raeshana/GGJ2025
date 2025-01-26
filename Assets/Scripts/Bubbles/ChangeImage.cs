93d1d2eef0697019b80d536611d641661dba32d0
Raeshana <rsookhoo@andrew.cmu.edu>
2025-01-26 13:11:25 -0500

    Instagram bubbles

---
+++ Assets/Scripts/Bubbles/ChangeImage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    [SerializeField] Image Image;
+    [SerializeField] Sprite[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get random indexes in transforms and bubblePrefabs
+        int imageIndex = Random.Range(0, images.Length-1); 
+        Image.sprite = images[imageIndex];
+    }
+}

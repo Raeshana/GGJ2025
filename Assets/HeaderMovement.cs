397ed622f3b0c0250156a7c7d381c4bf18f0f565
Cheyu Tu <cheyut@andrew.cmu.edu>
2025-01-26 08:25:19 -0500

    created header objects

---
+++ Assets/HeaderMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeaderMovement : MonoBehaviour
+{
+    public RectTransform blueHeader;
+    public RectTransform redHeader;
+
+    [Range(0f, 1f)]
+    public float blueScore = 0.5f;
+    private float previousBlueScore = -1f;
+    private float textWidth; // Text width is 90% of desktop width
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textWidth = Screen.width * 0.9f; // initialize text width
+        UpdateHeaders();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update headers only if blueScore changes
+        if (Mathf.Abs(blueScore - previousBlueScore) > Mathf.Epsilon)
+        {
+            UpdateHeaders();
+            previousBlueScore = blueScore;
+        }
+    }
+
+    void UpdateHeaders()
+    {
+        float redScore = 1f - blueScore;
+        blueHeader.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, blueScore * textWidth);
+        // blueHeader.anchoredPosition = new Vector2(0, blueHeader.anchoredPosition.y); // Ensure the blueHeader remains at the left edge
+        blueHeader.anchorMin = new Vector2(0, blueHeader.anchorMin.y);
+            blueHeader.anchorMax = new Vector2(0, blueHeader.anchorMax.y);
+            blueHeader.anchoredPosition = new Vector2(0, blueHeader.anchoredPosition.y);
+
+        redHeader.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, redScore * textWidth);
+        // redHeader.anchoredPosition = new Vector2(blueHeader.anchoredPosition.x + blueHeader.rect.width, redHeader.anchoredPosition.y); // Position redHeader just to the right of blueHeader
+        redHeader.anchorMin = new Vector2(0, redHeader.anchorMin.y);
+            redHeader.anchorMax = new Vector2(0, redHeader.anchorMax.y);
+            redHeader.anchoredPosition = new Vector2(blueHeader.rect.width, redHeader.anchoredPosition.y);
+    }
+}

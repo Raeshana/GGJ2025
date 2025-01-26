5f5ba2570754fa367e0236eb2e73f4096d1f5205
Raeshana <rsookhoo@andrew.cmu.edu>
2025-01-26 13:35:02 -0500

    Text Bubbles

---
+++ Assets/Scripts/Bubbles/ChangeSprite.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSprite : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get random indexes in transforms and bubblePrefabs
+        int spriteIndex = Random.Range(0, sprites.Length); 
+        sr.sprite = sprites[spriteIndex];
+    }
+}
